2e89f3c92f258ab0e69fab36f9160cccae51514b
Steivin <steivin2000@gmail.com>
2025-08-04 07:44:25 -0500

    instalando rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import DashboardPage from "./Pages/DashboardPage/DashboardPage";
+
+// Protege rutas con autenticación Firebase
+//import ProtectedRoute from "./Pages/Components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/*               Rutas publicas           */}
+        <Route path="/" element={<LoginPage />} />
+
+        {/*               Rutas privadas           
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <DashboardPage />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <UsersPage />{" "}
+            </ProtectedRoute>
+          }
+        />
+        */}
+
+        {/* Ruta genérica para páginas no encontradas 
+        <Route path="*" element={<NotFoundPage />} />
+        */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,10 @@
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <h1>Login Page</h1>
+      {/* Add your login form or components here */}
+    </div>
+  );
+}
+
+export default LoginPage;
